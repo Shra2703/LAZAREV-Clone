7179ccf7a07a6dd0f6c138cd89aa2591e3a8677e
shraddha goyal <shraddhagoyal800@gmail.com>
2024-04-01 22:55:29 +0530

    navbar animation part is completed

---
+++ script.js
@@ -0,0 +1,42 @@
+let nav = document.querySelector("nav");
+
+nav.addEventListener("mouseenter", () => {
+  let tl = gsap.timeline();
+  tl.to("#nav-bottom", {
+    height: "26vh",
+  });
+
+  tl.to("nav .nav-elem h5", {
+    display: "block",
+  });
+
+//   tl.from("nav .nav-elem h5 span", {
+//     y: 25,
+//     stagger: {
+//       amount: 0.3,
+//     },
+//   });
+});
+
+nav.addEventListener("mouseleave", () => {
+  let tl = gsap.timeline();
+  //   tl.to("#nav-bottom", {
+  //     height: "0vh",
+  //   });
+
+//   tl.to("nav .nav-elem h5 span", {
+//     y: 25,
+//     stagger: {
+//       amount: 0.2,
+//     },
+//   });
+  tl.to("nav .nav-elem h5", {
+    display: "none",
+    duration: 0.1,
+  });
+
+  tl.to("#nav-bottom", {
+    height: "0vh",
+    duration: 0.2,
+  });
+});
