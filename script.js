b127e050cb53ff14fd317a11e786459acfd93b54
shraddha goyal <shraddhagoyal800@gmail.com>
2024-04-03 23:51:38 +0530

    nav animation and moving animation is completed

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
-let nav = document.querySelector("nav");
+function navAnimation() {
+  let nav = document.querySelector("nav");
 
-nav.addEventListener("mouseenter", () => {
-  let tl = gsap.timeline();
-  tl.to("#nav-bottom", {
-    height: "26vh",
+  nav.addEventListener("mouseenter", () => {
+    let tl = gsap.timeline();
+    tl.to("#nav-bottom", {
+      height: "26vh",
+      duration: 0.5,
+    });
+
+    tl.to("nav .nav-elem h5", {
+      display: "block",
+      duration: 0.1,
+    });
+
+    tl.from("nav .nav-elem h5 span", {
+      y: 20,
+      stagger: {
+        amount: 0.3,
+      },
+    });
   });
 
-  tl.to("nav .nav-elem h5", {
-    display: "block",
+  nav.addEventListener("mouseleave", () => {
+    let tl = gsap.timeline();
+    //   tl.to("#nav-bottom", {
+    //     height: "0vh",
+    //   });
+
+    // tl.to("nav .nav-elem h5 span", {
+    //   y: 25,
+    //   stagger: {
+    //     amount: 0.2,
+    //   },
+    // });
+    tl.to("nav .nav-elem h5", {
+      display: "none",
+      duration: 0.1,
+    });
+
+    tl.to("#nav-bottom", {
+      height: "0vh",
+      duration: 0.2,
+    });
   });
+}
 
-//   tl.from("nav .nav-elem h5 span", {
-//     y: 25,
-//     stagger: {
-//       amount: 0.3,
-//     },
-//   });
-});
 
-nav.addEventListener("mouseleave", () => {
-  let tl = gsap.timeline();
-  //   tl.to("#nav-bottom", {
-  //     height: "0vh",
-  //   });
-
-//   tl.to("nav .nav-elem h5 span", {
-//     y: 25,
-//     stagger: {
-//       amount: 0.2,
-//     },
-//   });
-  tl.to("nav .nav-elem h5", {
-    display: "none",
-    duration: 0.1,
-  });
-
-  tl.to("#nav-bottom", {
-    height: "0vh",
-    duration: 0.2,
-  });
-});
+navAnimation();
